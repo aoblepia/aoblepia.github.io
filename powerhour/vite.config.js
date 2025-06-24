7804e4829beeec6bf3e84c3c78e486131fa62c98
aoblepia <116849986+aoblepia@users.noreply.github.com>
2025-06-24 01:42:30 -0500

    one more

---
+++ powerhour/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/powerhour/',   // <-- critical for GitHub Pages
+  plugins: [react()]
+});
